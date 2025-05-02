4e6d3d457e423ee407cb3e2efbc4072ff877a594
parizval <anmolgoyal@gmail.com>
2025-05-02 18:40:39 +0530

    added Factory and conditional interface

---
+++ src/interfaces/IConditionEvaultor.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+interface IConditionEvaultor {
+    function evaluateCondition(
+        uint8 _platform,
+        address _platformAddress,
+        address _borrower,
+        uint8 _parameter,
+        uint256 _conditionValue
+    ) external view returns (bool);
+}
---
+++ src/interfaces/IFactory.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+interface IFactory {
+    function emitOrderCreation() external;
+
+    function checkCondition(
+        uint8 _platform,
+        address _platformAddress,
+        address _borrower,
+        uint8 _parameter,
+        uint256 _conditionValue
+    ) external view returns (bool) ;
+}
