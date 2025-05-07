25a915d099daf643ea6fb94e49202559879945b3
parizval <anmolgoyal@gmail.com>
2025-05-07 22:47:00 +0530

    added IVault interface for order execution

---
+++ src/interfaces/IVault.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+struct OrderExecutionDetails {
+    address token;
+    address convert;
+    uint256 amount;
+    uint8 assetType;
+    uint16 platform;
+    bool repay;
+}
+
+interface IVault {
+    function decodeKey(bytes calldata orderId)
+        external
+        pure
+        returns (uint16 platform, address conditionAddress, uint16 parameter, uint32 destinationChainId, uint32 salt);
+
+    function getOrderExecutionDetails(bytes32 orderId)
+        external
+        view
+        returns (address _owner, OrderExecutionDetails memory order);
+
+    function executeOrder(bytes32 _orderId, address _solver) external payable;
+}
